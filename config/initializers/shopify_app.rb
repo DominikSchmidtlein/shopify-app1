6e11c992a97d6cdc23af583969db951d8d17cff4
Dominik Schmidtlein <dominik.schmidtlein@gmail.com>
2018-05-08 22:22:15 -0400

    use dotenv

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -1,7 +1,7 @@
 ShopifyApp.configure do |config|
   config.application_name = "My Shopify App"
-  config.api_key = "7587df4fa75a537492f2fa6d2c045760"
-  config.secret = "48c869ac24873bfa65f2fe712b24c290"
+  config.api_key = ENV['SHOPIFY_CLIENT_API_KEY']
+  config.secret = ENV['SHOPIFY_CLIENT_API_SECRET']
   config.scope = "read_orders, read_products"
   config.embedded_app = true
   config.after_authenticate_job = false
